92b234845a2ef6f06b4256aec43a505b1291996a
YITZULIU <s555478k@gmail.com>
2024-12-04 12:56:39 +0000

    update all

--- Seed/SeedApp.swift
+++ Seed/SeedApp.swift
@@ -24,8 +24,8 @@ struct SeedApp: App {
     let persistenceController = PersistenceController.shared
     var body: some Scene {
         WindowGroup {
-//            WelcomeView()
-            MeditationStreakView()
+            WelcomeView()
+//            MeditationStreakView()
             //.environment(\.managedObjectContext, persistenceController.container.viewContext)
 //            ActivitiesView()
                 .environment(\.modelContext, container.mainContext)

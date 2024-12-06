dabf3c9e06160376bd595d03fdc2f4d1e25d00a5
YITZULIU <s555478k@gmail.com>
2024-12-06 01:20:19 +0000

    add 資料傳遞，圖片

---
+++ Seed/Model/ElementForGarden.swift
@@ -0,0 +1,18 @@
+//
+//  ElementForGarden.swift
+//  Seed
+//
+//  Created by Yitzu Liu on 05/12/2024.
+//
+
+import SwiftData
+@Model // Annotation to indicate that this class is a model for data management / 注解，表示该类是数据管理模型
+class ElementForGarden {
+    var elementName : String
+    var isVisible : Bool
+    
+    init(elementName: String, isVisible: Bool) {
+        self.elementName = elementName
+        self.isVisible = isVisible
+    }
+}
